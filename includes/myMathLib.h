484407bbaa2a893de24194d1197a4e711fd699da
burstknight <burstknight@gmail.com>
2023-11-08 13:54:39 +0800

    feat: Add `myMathLib`
    - Target:
      This repo need add source code to practice `gtest`.
    
    - Changed:
      Add the lib `myMathLib`.

---
+++ includes/myMathLib.h
@@ -0,0 +1,6 @@
+#ifndef MY_MATH_LIB_H
+#define MY_MATH_LIB_H
+
+int add(int a, int b);
+
+#endif
---
+++ src/myMathLib.cpp
@@ -0,0 +1,6 @@
+#include "../includes/myMathLib.h"
+
+int add(int a, int b){
+	return a + b;
+} // End of add
+
